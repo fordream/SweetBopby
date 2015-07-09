688485fa20d8a2de995fc9055c8e1f723e8f8251
clessmax <vuongvantruong1987@gmail.com>
2015-07-09 16:48:42 +0700

    add

---
+++ Sweet Bopby/src/game/base/ItemObject.java
@@ -0,0 +1,79 @@
+package game.base;
+
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.sprite.AnimatedSprite;
+import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
+
+import android.graphics.Point;
+
+public class ItemObject {
+	private Point position = new Point();
+	private AnimatedSprite backgroud;
+	private AnimatedSprite sprite;
+	// type = -1 0 1 2 3 4 5 6 7 8
+	private int type = -1;
+
+	public Point getPosition() {
+		return position;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void create(Scene scene, int left, int top, int positionx, int positionY, TiledTextureRegion region) {
+		position.x = positionx;
+		position.y = positionY;
+		int with = region.getWidth()/2;
+		int height = region.getHeight();
+		backgroud = new AnimatedSprite(left + positionx * with, top + positionY * height, region.deepCopy());
+		scene.attachChild(backgroud);
+	}
+
+	public void clear(Scene scene) {
+		type = -1;
+		isChecked = false;
+		// backgroud.animate(0);
+		if (sprite != null) {
+			scene.detachChild(sprite);
+		}
+		sprite = null;
+	}
+
+	public void randomType(Scene scene, int type, BaseMSprise[] bigs) {
+		this.type = type;
+		sprite = new AnimatedSprite(backgroud.getX(), backgroud.getY(), bigs[type - 1].getRegCat());
+		scene.attachChild(sprite);
+	}
+
+	public boolean isSelected(int x, int y) {
+		return backgroud.getX() < x && x < backgroud.getX() + backgroud.getWidth() / 2 && backgroud.getY() < y && y < backgroud.getY() + backgroud.getHeight();
+	}
+
+	public void setChecked(boolean isChecked) {
+		if (isBig()) {
+			this.isChecked = isChecked;
+		}
+	}
+
+	public void runAnimation(boolean isRun) {
+		// FIXME
+		// run animation selected
+		if (isChecked()) {
+			backgroud.animate(100);
+		} else {
+			backgroud.stopAnimation();
+			backgroud.setCurrentTileIndex(0);
+		}
+	}
+
+	private boolean isChecked = false;
+
+	public boolean isChecked() {
+		return isChecked;
+	}
+
+	public boolean isBig() {
+		return type <= 8;
+	}
+}
--- Sweet Bopby/src/org/vnp/sweetbopby/MainActivity.java
+++ Sweet Bopby/src/org/vnp/sweetbopby/MainActivity.java
@@ -2,35 +2,124 @@ package org.vnp.sweetbopby;
 
 import game.base.BaseMGameActivty;
 import game.base.BaseMSprise;
+import game.base.ItemObject;
+
+import java.util.Random;
 
 import org.anddev.andengine.entity.scene.Scene;
-import org.anddev.andengine.entity.sprite.AnimatedSprite;
 import org.anddev.andengine.input.touch.TouchEvent;
-
-import com.vnp.core.common.CommonAndroid;
+import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
 
 import android.os.Bundle;
-import android.view.Display;
 
 public class MainActivity extends BaseMGameActivty {
 	private BaseMSprise baseSprise = new BaseMSprise();
+	private ItemObject[][] boards = new ItemObject[9][9];
+
+	private BaseMSprise[] bigs = new BaseMSprise[8];
+	private BaseMSprise[] smalls = new BaseMSprise[8];
+
+	@Override
+	protected void onCreate(Bundle pSavedInstanceState) {
+		super.onCreate(pSavedInstanceState);
+
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				boards[i][j] = new ItemObject();
+			}
+		}
+
+		for (int i = 0; i < 8; i++) {
+			smalls[i] = new BaseMSprise();
+			bigs[i] = new BaseMSprise();
+		}
+	}
 
 	@Override
 	public boolean onSceneTouchEvent(Scene arg0, TouchEvent arg1) {
+
+		if (arg1.getAction() == TouchEvent.ACTION_DOWN) {
+			int x = (int) arg1.getX();
+			int y = (int) arg1.getY();
+			// null or not null
+			ItemObject itemSelected = getItemSlected(x, y);
+			// null or not null
+			ItemObject itemChecked = getItemSlected();
+
+			if (itemChecked == null) {
+				if (itemSelected != null) {
+					itemSelected.setChecked(true);
+					itemSelected.runAnimation(true);
+				}
+			} else if (itemSelected != null) {
+				if (itemSelected == itemChecked) {
+					itemSelected.setChecked(false);
+					itemSelected.runAnimation(false);
+				} else if (!itemSelected.isBig()) {
+					// FIXME find way
+				}
+			}
+		}
+
 		return false;
+	}
+
+	private ItemObject getItemSlected() {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (boards[i][j].isChecked()) {
+					return boards[i][j];
+				}
+			}
+		}
+		return null;
+	}
+
+	private ItemObject getItemSlected(int x, int y) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (boards[i][j].isSelected(x, y)) {
+					return boards[i][j];
+				}
+			}
+		}
+		return null;
 	}
 
 	@Override
 	public void onLoadComplete() {
-		int with = baseSprise.getRegCat().getWidth();
-		int height = baseSprise.getRegCat().getHeight();
-		int left = ((int)getmCamera().getWidth()- with * 9) / 2;
-		int top = ((int)getmCamera().getHeight()- height * 9) / 2;
+		TiledTextureRegion region = baseSprise.getRegCat();
+		int with = region.getWidth() / 2;
+		int height = region.getHeight();
+		int left = ((int) getmCamera().getWidth() - with * 9) / 2;
+		int top = ((int) getmCamera().getHeight() - height * 9) / 2;
 
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
-				AnimatedSprite sprite = new AnimatedSprite(left + i * with, top + j * height, baseSprise.getRegCat().deepCopy());
-				getmMainScene().attachChild(sprite);
+				boards[i][j].create(getmMainScene(), left, top, i, j, region);
+			}
+		}
+
+		createNewGame();
+	}
+
+	public void createNewGame() {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				boards[i][j].clear(getmMainScene());
+			}
+		}
+
+		Random random = new Random();
+
+		for (int i = 0; i < 3; i++) {
+			int px = random.nextInt(9);
+			int py = random.nextInt(9);
+
+			if (boards[px][py].getType() != -1) {
+				i--;
+			} else {
+				boards[px][py].randomType(getmMainScene(), random.nextInt(8) + 1, bigs);
 			}
 		}
 	}
@@ -38,7 +127,11 @@ public class MainActivity extends BaseMGameActivty {
 	@Override
 	public void onLoadResources() {
 		super.onLoadResources();
-		baseSprise.onCreateResources(mEngine, this, "mbg.png", 1, 1);
+		baseSprise.onCreateResources(mEngine, this, "mbg.png", 2, 1);
+		for (int i = 0; i < 8; i++) {
+			smalls[i].onCreateResources(mEngine, this, (i + 1) + "_small.png", 1, 1);
+			bigs[i].onCreateResources(mEngine, this, (i + 1) + ".png", 1, 1);
+		}
 	}
 
 	@Override
@@ -46,5 +139,4 @@ public class MainActivity extends BaseMGameActivty {
 		baseSprise.onCreateScene(getmMainScene());
 		return super.onLoadScene();
 	}
-
 }

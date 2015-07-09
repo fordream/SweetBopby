2d28638431f84080a7e57a2e9a0e8b24504016c4
clessmax <vuongvantruong1987@gmail.com>
2015-07-09 14:11:36 +0700

    add

--- Sweet Bopby/src/org/vnp/sweetbopby/MainActivity.java
+++ Sweet Bopby/src/org/vnp/sweetbopby/MainActivity.java
@@ -1,16 +1,36 @@
 package org.vnp.sweetbopby;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import game.base.BaseMGameActivty;
+import game.base.BaseMSprise;
 
-public class MainActivity extends Activity {
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.input.touch.TouchEvent;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseMGameActivty {
+	private BaseMSprise baseSprise = new BaseMSprise();
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	public boolean onSceneTouchEvent(Scene arg0, TouchEvent arg1) {
+		return false;
+	}
+
+	@Override
+	public void onLoadComplete() {
+		getmMainScene().attachChild(baseSprise.getSprCat());
+	}
+
+	@Override
+	public void onLoadResources() {
+		super.onLoadResources();
+		baseSprise.onCreateResources(mEngine, this, "mbg.png", 1, 1);
+	}
+
+	@Override
+	public Scene onLoadScene() {
+		baseSprise.onCreateScene(getmMainScene());
+		return super.onLoadScene();
 	}
 
 }
